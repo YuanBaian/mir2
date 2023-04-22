84ee2a185793a778c8e2c4250cc1755208ef4129
Yuanbaian <79123626@qq.com>
2023-04-23 03:59:34 +0800

    dpr file

--- Server/Server.dpr
+++ Server/Server.dpr
@@ -5,7 +5,12 @@ uses
   Main in 'MirForm\Main.pas' {SMain},
   Packet in '..\Shared\Packet.pas',
   Enum in '..\Shared\Enum.pas',
-  ServerPackets in '..\Shared\ServerPackets.pas';
+  ServerPackets in '..\Shared\ServerPackets.pas',
+  SelectInfo in '..\Shared\Data\SelectInfo.pas',
+  ClientData in '..\Shared\Data\ClientData.pas',
+  ItemData in '..\Shared\Data\ItemData.pas',
+  SharedData in '..\Shared\Data\SharedData.pas',
+  State in '..\Shared\Data\State.pas';
 
 {$R *.res}
 
